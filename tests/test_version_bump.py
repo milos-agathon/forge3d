c290dc22d642f7c7ad38fccbbfc47a88bc96f770
Milos Popovic <milos.agathon@gmail.com>
2025-09-04 08:35:18 +0200

    Changed tests

--- tests/test_version_bump.py
+++ tests/test_version_bump.py
@@ -1,6 +1,6 @@
-def test_version_reports_050():
+def test_version_reports_060():
     import forge3d as f3d
-    assert f3d.__version__ == "0.5.0"
+    assert f3d.__version__ == "0.6.0"
     # compiled module should match
     from forge3d import _forge3d as core
-    assert core.__version__ == "0.5.0"
+    assert core.__version__ == "0.6.0"
